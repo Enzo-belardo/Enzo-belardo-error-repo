7175d726f434b967ac85f300c636ac8cb9b7262a
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-01-24 18:32:30 +0100

    add: form

---
+++ convalida.php
@@ -0,0 +1,12 @@
+
+<h3>
+   <?php
+      echo $_POST['testo'];
+   ?>
+</h3>
+
+<h3>
+   <?php
+      echo $_POST['parola'];
+   ?>
+</h3>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form</title>
+</head>
+<body>
+    <main>
+        <form action="/esercizi-php/convalida.php" method="POST">
+            <label for="testo">scrivi del testo</label>
+            <textarea name="testo" id="" cols="40" rows="10"></textarea>
+            <input type="text" name="parola">
+            <button type="submit">invia</button>
+        </form>
+    </main> 
+</body>
+</html>
